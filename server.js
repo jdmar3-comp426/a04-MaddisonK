8ad50a3bdef5dbe8151532386810ccbb859bd75d
MaddisonK <maddk@live.unc.edu>
2021-12-02 21:30:17 -0500

    single line stmt

--- server.js
+++ server.js
@@ -24,8 +24,7 @@ app.get("/app/", (req, res, next) => {
 // Define other CRUD API endpoints using express.js and better-sqlite3
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
 app.post("/app/new/", (req, res) => {
-	const stmt = db.prepare("INSERT INTO userinfo (user,pass) VALUES (?,?)");
-	const info = stmt.run(req.body.user, req.body.pass);
+	const stmt = db.prepare("INSERT INTO userinfo (user,pass) VALUES (?,?)").run(req.body.user, req.body.pass);
 	res.status(201).json({"message":"1 record created: ID " + info.lastInsertRowid + " (201)"});
 	
 });
