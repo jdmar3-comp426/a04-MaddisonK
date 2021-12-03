dd00b6f386f04ee67e85681960256f5cfb44f81e
MaddisonK <maddk@live.unc.edu>
2021-12-02 21:49:10 -0500

    fixed prints

--- server.js
+++ server.js
@@ -23,11 +23,11 @@ app.get("/app/", (req, res, next) => {
 
 // Define other CRUD API endpoints using express.js and better-sqlite3
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
-app.post("/app/new/", (req, res) => {
-	const stmt = db.prepare("INSERT INTO userinfo (user,pass) VALUES (?,?)").run(req.body.user, req.body.pass);
-	res.status(201).json({"message": "1 record created: ID " + stmt.lastInsertRowid + " (201)"});
-	
-});
+app.post("/app/new/", (req, res)=>{
+	const stmt = db.prepare("INSERT INTO userinfo (user,pass) VALUES (?,?)").run(req.body.user, md5(req.body.pass));
+	res.status(201).json({"message":"1 record created: ID %ID% (201)".replace("%ID%", stmt.lastInsertRowid)});
+	//res.status(201).json({"message": "1 record created: ID " + stmt.lastInsertRowid + " (201)"});
+})
 
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
 app.get("/app/users", (req, res) => {	
@@ -43,9 +43,10 @@ app.get("/app/user/:id", (req, res) => {
 });
 
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
-app.put("/app/update/user/:id", (req, res) => {
-	const info = db.prepare("UPDATE userinfo SET user = ?, pass = ? WHERE id = ?").run(req.body.user, md5(req.body.pass), req.params.id);
-	res.status(201).json({"message": "1 record updated: ID " + info.lastInsertRowid + " (200)"});
+app.patch("/app/update/user/:id", (req, res)=>{
+	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?").run(req.body.user, md5(req.body.pass), req.params.id);
+	res.status(201).json({"message":"1 record updated: ID %ID% (200)".replace("%ID%", req.params.id)});
+	//res.status(201).json({"message": "1 record updated: ID " + info.lastInsertRowid + " (200)"});
 });
 // req.params.id
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
